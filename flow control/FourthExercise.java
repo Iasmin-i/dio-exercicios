695ba093d39aa4e2355e1ce3efcec18a91c806d7
Iasmin-i <7.nimsai@gmail.com>
2025-06-30 19:37:55 -0300

    Enviando arquivos

--- flow control/FourthExercise.java
+++ flow control/FourthExercise.java
@@ -8,5 +8,25 @@ public class FourthExercise {
      * Continuar até que o resto da divisão resulte em um número diferente de zero
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int result = 0;
+        
+        do {
+            int secondNumber = scanner.nextInt();
+            
+            if(firstNumber > secondNumber){
+                result = (firstNumber % secondNumber);
+                System.out.println(result);
+            } else{
+                System.out.println("The first number must be higher than the second");
+                continue;
+            }
+            
+        } while(result == 0);
+        
+        System.out.println("Successfully executed");
+        scanner.close();
+        
     }
-    
+}
